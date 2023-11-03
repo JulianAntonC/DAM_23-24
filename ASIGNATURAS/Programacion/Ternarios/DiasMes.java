454dd48235e4b482fa6e2637b35633dad74902af
JulianAntonC <julantcol@alu.edu.gva.es>
2023-11-03 13:08:31 +0100

    0.0.5v

---
+++ ASIGNATURAS/Programacion/Ternarios/DiasMes.java
@@ -0,0 +1,38 @@
+public class DiasMes {
+	public static void main (String [] args) {
+	
+		System.out.print("\nIntroduzca el año que desea conocer: ");
+		int fechaAnual = Integer.parseInt(System.console().readLine());
+		
+		System.out.print("\nIntroduzca el mes que desea conocer: ");
+		int fechaMes = Integer.parseInt(System.console().readLine());
+		
+		switch(fechaMes) {
+			
+			case 1,3,5,7,8,10,12:
+			
+				System.out.println("Tiene 31 días.");
+				break;
+			
+			case 4,6,9,11:
+			
+				System.out.println("Tiene 30 días.");
+				break;
+			
+			case 2:
+			
+				if((fechaAnual % 4 == 0) && (fechaAnual % 100 != 0) || (fechaAnual % 400 == 0)) { 
+				
+				System.out.println("Tiene 29 días.");
+				
+				} else {
+					
+					System.out.println("Tiene 28 días.");
+					
+				}
+				
+				break;
+		}
+		
+	}
+}
--- ASIGNATURAS/Programacion/Ternarios/RepeticionLetra.java
+++ ASIGNATURAS/Programacion/Ternarios/RepeticionLetra.java
@@ -1,13 +1,38 @@
-public class RepiticionLetra {
+public class RepeticionLetra {
 	public static void main (String [] args) {
 		
-		int letra;
+		int letra = 0;
+		int loop = 0;
 		
+		//Solicitud de una oración.
 		System.out.println("Introduzca una oración: \n");
-		String entrada = System.console().readLine();
+		String frase = System.console().readLine();
 		
+		//Solicitud de la letra a encontrar.
 		System.out.println("\nIntroduzca la letra que desea encontrar: \n");
-		String entrada = System.console().readLine();
+		String letter = System.console().readLine();
+		
+		
+		//Dentro de una oración, hallar una letra en especifico que ha sido introducida previamente por el usuario, esa letra ha de mostrarse en pantalla el numero de veces que aparece en pantalla
+		
+		//PosicionFrase(posFr)
+		for(int posFr = 0; posFr < frase.length(); posFr++) {
+			
+			if(frase.substring(posFr, posFr + 1).equalsIgnoreCase(letter)) {
+				letra++;
+			}
+			
+			loop++;
+			
+		}
+		
+		//Test
+		System.out.println("\n" + frase);
+		System.out.println(letter);
+		System.out.println(letra);
+		System.out.println(loop);
+		
+		
 		
 	}
 }
